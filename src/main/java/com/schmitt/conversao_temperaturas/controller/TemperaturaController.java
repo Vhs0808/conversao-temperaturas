e7da43d07d5084c0f6e0782c61c27313c13625be
Vhs0808 <schmittvitorhugo2@gmail.com>
2026-09-04 21:49:36 -0300

    feat: Inicia criação de controller de conversão

--- src/main/java/com/schmitt/conversao_temperaturas/controller/TemperaturaController.java
+++ src/main/java/com/schmitt/conversao_temperaturas/controller/TemperaturaController.java
@@ -1,13 +1,23 @@
 package com.schmitt.conversao_temperaturas.controller;
 
+import com.schmitt.conversao_temperaturas.dto.ConversaoRequestDto;
+import com.schmitt.conversao_temperaturas.dto.ConversaoResponseDto;
+import com.schmitt.conversao_temperaturas.service.ConversaoService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/temperaturas")
+@AllArgsConstructor
 @RestController
+@RequestMapping("/temperaturas")
 public class TemperaturaController {
 
-    @PostMapping
-    public
+    private final ConversaoService conversaoService;
+
+    @PostMapping("/converter")
+    public ConversaoResponseDto converteTemperatura(@RequestParam ConversaoRequestDto request){
+        return conversaoService.converteTemperatura(request);
+    }
 }
