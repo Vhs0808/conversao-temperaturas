5257698911d3e071cc34dcae0251402fa73cf9c8
Vhs0808 <schmittvitorhugo2@gmail.com>
2026-09-04 21:48:42 -0300

    fix: Arruma dto de requests de conversão

--- src/main/java/com/schmitt/conversao_temperaturas/dto/ConversaoRequestDto.java
+++ src/main/java/com/schmitt/conversao_temperaturas/dto/ConversaoRequestDto.java
@@ -2,10 +2,12 @@ package com.schmitt.conversao_temperaturas.dto;
 
 import com.schmitt.conversao_temperaturas.enuns.TemperaturasUnidade;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class ConversaoRequestDto {
     private double valor;
     private TemperaturasUnidade unidadeOrigem;
