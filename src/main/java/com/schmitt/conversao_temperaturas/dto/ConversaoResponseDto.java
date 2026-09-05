a4bd348a30e473a4151dae768bc9326093741cfd
Vhs0808 <schmittvitorhugo2@gmail.com>
2026-09-04 21:25:44 -0300

    feat: Implementa classe dto de resposta para temperatura

--- src/main/java/com/schmitt/conversao_temperaturas/dto/ConversaoResponseDto.java
+++ src/main/java/com/schmitt/conversao_temperaturas/dto/ConversaoResponseDto.java
@@ -1,4 +1,12 @@
 package com.schmitt.conversao_temperaturas.dto;
 
+import com.schmitt.conversao_temperaturas.enuns.TemperaturasUnidade;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class ConversaoResponseDto {
+    private double temperatura;
+    private TemperaturasUnidade unidade;
 }
